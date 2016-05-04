9dbb2930d72074d66656c56eb2203b95c9b0ee7c
Ronan Boiteau <ronan.boiteau@epitech.eu>
2016-05-04 17:41:30 +0200

    Fix main.c

--- src/main.c
+++ src/main.c
@@ -5,12 +5,19 @@
 ** Login   <boitea_r@epitech.net>
 ** 
 ** Started on  Wed May  4 16:16:08 2016 Ronan Boiteau
-** Last update Wed May  4 16:16:27 2016 Ronan Boiteau
+** Last update Wed May  4 17:40:45 2016 Ronan Boiteau
 */
 
 #include <stdlib.h>
+#include "my.h"
 
 int		main(void)
 {
+  my_printf("%s", "START_SIMULATION\n");
+  while (42)
+    {
+      my_printf("%s", "CAR_FORWARD:0.5\n");
+    }
+  my_printf("%s", "STOP_SIMULATION\n");
   return (EXIT_SUCCESS);
 }
